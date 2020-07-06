ad65c1bee6a38f31953fb9f5aac32f8a7990a4ee
wmolicki <wojtek.molicki@gmail.com>
2020-07-06 23:38:38 +0200

    Move rest of the handler code to handlers package

--- src/main.go
+++ src/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
-	"text/template"
 
 	"go-url-shortener/dal"
 	"go-url-shortener/handlers"
@@ -16,62 +14,24 @@ func main() {
 	db := dal.MustGetDb()
 	router := mux.NewRouter()
 
-	indexTemplate := template.Must(template.ParseFiles("static/index.html"))
 	baseUrl := "localhost:8080"
 
 	router.HandleFunc(
 		"/status", handlers.StatusHandler(db)).Methods(http.MethodGet)
 
 	router.HandleFunc(
-		"/", func(w http.ResponseWriter, r *http.Request) {
-			// will serve the main page
-			indexTemplate.Execute(w, nil)
-		}).Methods(http.MethodGet)
+		"/", handlers.IndexHandler()).Methods(http.MethodGet)
+
+	router.HandleFunc(
+		"/", handlers.CreateShortenedURLHandler(db)).Methods(http.MethodPost)
 
 	router.Handle(
 		"/success", &handlers.SuccessHandler{BaseUrl: baseUrl}).Methods(http.MethodGet)
 
 	router.HandleFunc(
-		"/", func(w http.ResponseWriter, r *http.Request) {
-			// will save posted url as new
-			err := r.ParseForm()
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				w.Write([]byte(fmt.Sprintf("%v", err)))
-			}
+		"/{urlId}", handlers.RedirectHandler(db)).Methods(http.MethodGet)
 
-			url := r.FormValue("url")
-			if url == "" {
-				w.WriteHeader(http.StatusBadRequest)
-				w.Write([]byte("You have to provide nonempty url"))
-			}
-			if strings.HasPrefix(url, "https://") == false {
-				url = "https://" + url
-			}
-
-			userAgent := r.Header.Get("User-Agent")
-			shortened := dal.InsertShortenedURL(db, url, userAgent)
-
-			http.Redirect(w, r, fmt.Sprintf("/success?s=%s", shortened), http.StatusSeeOther)
-
-		}).Methods(http.MethodPost)
-
-	router.HandleFunc(
-		"/{urlId}", func(w http.ResponseWriter, r *http.Request) {
-			// will redirect to the shortened url, or 404 if not registered
-			vars := mux.Vars(r)
-			urlId := vars["urlId"]
-
-			redirectURL, exists := dal.GetOriginalURL(db, urlId)
-
-			if exists == false {
-				w.WriteHeader(http.StatusNotFound)
-				return
-			}
-
-			http.Redirect(w, r, redirectURL, http.StatusTemporaryRedirect)
-
-		}).Methods(http.MethodGet)
+	
 
 	err := http.ListenAndServe(baseUrl, router)
 	if err != nil {
